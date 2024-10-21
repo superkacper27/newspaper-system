2aea21bedc36fb4b929140b6d4a8d007970090a5
Kacper-with-a-K <58475524+superkacper27@users.noreply.github.com>
2024-10-21 15:34:05 +0100

    SQL Expanded

--- NewspaperDeliverySystem/src/Customer/customer.java
+++ NewspaperDeliverySystem/src/Customer/customer.java
@@ -1,5 +1,8 @@
 package Customer;
 
 public class customer {
-
-}
+	public static void main(String[] args)
+	{
+		
+		}
+	}
---
+++ NewspaperDeliverySystem/src/MySql/MySqlAccess.java
@@ -0,0 +1,55 @@
+package MySql;
+
+public class MySqlAccess {
+	
+	private Connection connect = null;
+	private Statement statement = null;
+	private PreparedStatement preparedStatement = null;
+	private ResultSet resultSet = null;
+	
+	final private String host ="localhost:3306";
+	final private String user = "root";
+	final private String password = "mike";
+	
+	
+	public MySqlAccess() throws Exception {
+		
+		try {
+			
+			//Load MySQL Driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			//Setup the connection with the DB
+			connect = DriverManager.getConnection("jdbc:mysql://" + host + "/newsagent2021?" + "user=" + user + "&password=" + password);
+		}
+		catch (Exception e) {
+			throw e;
+		}
+		
+		
+	}	
+
+	public boolean insertCustomerDetailsAccount(Customer c) {
+	
+		boolean insertSucessfull = true;
+	
+		//Add Code here to call embedded SQL to insert Customer into DB
+	
+		try {
+		
+			//Create prepared statement to issue SQL query to the database
+			preparedStatement = connect.prepareStatement("insert into newsagent2021.customer values (default, ?, ?, ?)");
+			preparedStatement.setString(1, c.getName());
+			preparedStatement.setString(2, c.getAddress());
+			preparedStatement.setString(3, c.getPhoneNumber());
+			preparedStatement.executeUpdate();
+		
+	 
+		}
+		catch (Exception e) {
+			insertSucessfull = false;
+		}
+	
+		return insertSucessfull;
+		
+	}
